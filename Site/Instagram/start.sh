358fca0a90282a1823c8d7aa841ee06db2a10368
Nihat 「 FARZ 」 <99778063+NihatFarz@users.noreply.github.com>
2022-03-09 03:05:24 +0400

    「 FARZ 」

--- Site/Instagram/start.sh
+++ Site/Instagram/start.sh
@@ -1,31 +1,29 @@
 #!/bin/bash|
-#TEST
-#Developer:Nihat 「 FARZ 」
-#İnstagram : @NihatFarz
-#Telegram : NihatFarz
+#Code:Yaman Efkar
+#Türk Siber Timi
+#İnstagram : @turksibertimi
+#Youtube : YamanEfkar
 
 clear
 
 echo -e '\033[32;40;1m
-        ███████╗ █████╗ ██████╗ ███████╗
-        ██╔════╝██╔══██╗██╔══██╗╚══███╔╝
-        █████╗  ███████║██████╔╝  ███╔╝ 
-        ██╔══╝  ██╔══██║██╔══██╗ ███╔╝  
-        ██║     ██║  ██║██║  ██║███████╗
-        ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
-        
-	
+        /\︵-︵/\
+        |( ◉)(◉)|
+        \ ︶V︶ /
+        /↺↺↺↺↺↺↺\
+        ↺↺↺↺↺↺↺↺|
+        \↺↺↺↺↺↺↺/
+        ¯¯/\¯/\¯¯ '
+
 
 
 echo -e "\033[31;40;1m
 \033[31;40;1m[01]\e[37mİnstagram Phishing          \033[31;40;1m[02]\e[37mİnstagram Brute-Force
 \033[31;40;1m[03]\e[37mİnstagram Fake Panel
-
-
-\e[31m[00]\e[37m Menyu
-\e[31m[99]\e[37m Çıxış
+\e[31m[00]\e[37m Menü
+\e[31m[99]\e[37m Exit
 "
-echo -e "\033[32;40;1m                     Nihat 「 FARZ 」"
+echo -e "\033[32;40;1m                      Nihat 「 FARZ 」"
 echo ""
 read -p "Seçim Edin : " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
@@ -41,7 +39,7 @@ elif [[ $islem == 2 || $islem == 02 ]]; then
         cd  Instagram-bruteforce/
         read -p "Hədəf İstifadəçinin Username : " username
         echo ""
-        read -p "Wordlist'in olduğu bölmə : " password
+        read -p "Wordlist-in olduğu bölmə : " password
         python3 instagram.py $username $password -m 3
 
 elif [[ $islem == 3 || $islem == 03 ]]; then
@@ -62,11 +60,11 @@ elif [[ $islem == 3 || $islem == 03 ]]; then
 elif [[ $islem == 00 ]]; then
 	cd ..
 	cd ..
-	bash farz.sh
+	bash tst.sh
 
 
 else
-	echo -e '\033[31;40;1m Seçiminizi düzgün edin!'
+	echo -e '\033[31;40;1m Seçiminizi Düzəldin!'
 	sleep 2
 	clear
 	bash start.sh
